f793ee02453cd7be28dc5b9c9f467207770b335c
Jon Mulieri <jonathan.mulieri@gmail.com>
2019-09-29 14:13:08 -0700

    Adding basic result set caching for search and near actions based on request params

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -4,13 +4,17 @@ class FacilitiesController < ApplicationController
   before_action :verify_nearness_params, only: :near
 
   def search
-    table = Facility.arel_table
-    @facilities = Facility.where(table[:zip_code].matches("#{sanitized_zip_code}%"))
+    @facilities = Rails.cache.fetch("search|#{sanitized_zip_code}", expires_in: 12.hours) do
+      table = Facility.arel_table
+      Facility.where(table[:zip_code].matches("#{sanitized_zip_code}%"))
+    end
     json_response(@facilities)
   end
 
   def near
-    @facilities = Facility.near(params[:location], params[:radius].to_f, latitude: :lat, longitude: :lng)
+    @facilities = Rails.cache.fetch("near|#{params[:location]}|#{params[:radius]}", expires_in: 12.hours) do
+      Facility.near(params[:location], params[:radius].to_f, latitude: :lat, longitude: :lng)
+    end
     json_response(@facilities)
   end
 
